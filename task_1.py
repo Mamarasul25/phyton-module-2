bc5812e50c97595c127062d8f17690283f4f93db
Mamarasul25 <44110566+Mamarasul25@users.noreply.github.com>
2022-02-13 10:06:18 +0000

    Initial commit

---
+++ task_1.py
@@ -0,0 +1,6 @@
+print('Задача 1. Пропавшая переменная')
+client = 'Петя'
+print(client)
+print('и')
+pet = "Кот"
+print(pet)  
---
+++ task_7.py
@@ -0,0 +1,4 @@
+print('Задача 7. Путь к файлу')
+user = input('Введите ваше имя: ')
+file_name = input('Введите имя файла: ')
+print('C:/'+ 1 + '/docs/folder/' + file_name + '.txt')
